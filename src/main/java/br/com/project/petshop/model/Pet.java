3780f044deaa300515d74f910dbf95c5ef31ece5
Carlos Ueda <carlos.ueda@gmail.com>
2019-09-19 05:54:13 -0400

    Creating Pet entity

---
+++ src/main/java/br/com/project/petshop/model/Pet.java
@@ -0,0 +1,22 @@
+package br.com.project.petshop.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="pet")
+@Data
+public class Pet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String breed;
+}
